2ce13c6c1dccdfc53dff7e3e24dd171fe7bcb71d
josimar silva <josimar.os23@gmail.com>
2024-09-03 09:14:59 -0300

    deleting environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        app_id: "1856737",
-        key: "dff0de29756fc8f37c45",
-        secret: "5b7fbfc34974c9121ab6",
-        cluster: "us2",
-        email: "bingo.supabase.pusher@outlook.com",
-        email_pass: "bingo@supabase@pusher",
-        supabase_pass: "bingo@supabase@pusherA0",
-        supabase_project_pass:"NJ1MUhZDjPsUPdbb",
-        NEXT_PUBLIC_SUPABASE_URL: 'https://clfgfbskcdmjgibsedan.supabase.co',
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsZmdmYnNrY2RtamdpYnNlZGFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4Njg1OTQsImV4cCI6MjA0MDQ0NDU5NH0.NC5jRzxe8NT50U88FQHrMof6Gs_Z1JiJUSU-rmBDEqU'
+        app_id: process.env.APP_ID,
+        key:process.send.KEY,
+        secret:process.send.SECRETE,
+        cluster:process.send.CLUSTER,
+        email:process.send.EMAIL,
+        email_pass:process.send.EMAIL_PASS,
+        supabase_pass:process.send.SUPABASE_PASS,
+        supabase_project_pass:process.send.SUPABASE_PROJECT_PASS,
+        NEXT_PUBLIC_SUPABASE_URL:process.send.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY:process.send.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     }
 };
 
